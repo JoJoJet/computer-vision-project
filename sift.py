7591f7d9d90b96800960785c15dab1512cac6a45
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2023-04-26 23:14:09 -0400

    update a local function

--- sift.py
+++ sift.py
@@ -27,31 +27,33 @@ def get_sift_matches(sift, image, template_image):
                         flags=cv2.DrawMatchesFlags_DEFAULT)
     img3 = cv2.drawMatchesKnn(image,kp1,template_image,kp2,matches,None,**draw_params)
     cv2.imshow('sift',img3)
-    
-    # Rounds a transform into a set of bins representing the
-    # two nearest bins in each dimension (total of sixteen bins).
-    # Theta should be measured in degrees.
-    def into_bins(x, y, theta):
-        x1 = floor(x * 100) / 100
-        x2 = ceil(x * 100) / 100
-        y1 = floor(y * 100) / 100
-        y2 = ceil(y * 100) / 100
-        theta1 = floor(theta * 30) / 30
-        theta2 = ceil(theta * 30) / 30
-        return [x1, x2], [y1, y2], [theta1, theta2]
         
     if len(good_matches) == 0:
         return []
     
+    # Rounds a transform into a set of bins representing the
+    # two nearest bins in each dimension.
+    # Theta should be measured in degrees.
+    def into_bins(x, y, s, theta):
+        x1 = floor(x * 100) / 100
+        x2 = ceil(x * 100) / 100
+        y1 = floor(y * 100) / 100
+        y2 = ceil(y * 100) / 100
+        s1 = 2**(floor(log2(s)))
+        s2 = 2**(ceil(log2(s)))
+        theta1 = floor(theta * 30) / 30
+        theta2 = ceil(theta * 30) / 30
+        return [x1, x2], [y1, y2], [s1, s2] [theta1, theta2]
+    
     # For each matched feature, vote for its nearby hough bins.
     hough = {}
     for i,(m,n) in enumerate(good_matches):
-        dx, dy, d_theta = into_bins(n.pt[0]-m.pt[0], n.pt[1]-m.pt[1], n.angle-m.angle)
-        ds = 2**(n.octave-m.octave)
+        dx, dy, ds, d_theta = into_bins(n.pt[0]-m.pt[0], n.pt[1]-m.pt[1], n.size/m.size, n.angle-m.angle)
         for x in dx:
             for y in dy:
-                for theta in d_theta:
-                    hough.setdefault((x,y,ds,theta),[]).append(i)
+                for s in ds
+                    for theta in d_theta:
+                        hough.setdefault((x,y,ds,theta),[]).append(i)
     
     # Find the bin with the most votes.
     best_bin = max(hough, key = lambda k: len(hough[k]))
